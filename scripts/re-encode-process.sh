7e7cdd77ad9467f6ce9274768edd3f0892e7876d
josh <joshua2allen@gmail.com>
2023-07-15 17:48:22 -0500

    checked 720 settings to 28

--- scripts/re-encode-process.sh
+++ scripts/re-encode-process.sh
@@ -37,7 +37,7 @@ if [ ! -f "$fullPath" ]
 then 
     echo no video files found in $inputPath
     $inputPath=$basePath/input-720
-    ffmpegOptions='-c:v libx264 -crf 30 -vf scale=-1:720 -movflags faststart -preset veryfast'
+    ffmpegOptions='-c:v libx264 -crf 28 -vf scale=-1:720 -movflags faststart -preset veryfast'
     fullPath=$(find $inputPath -type f \( -iname \*.mp4 -o -iname \*.webm -o -iname \*.avi -o -iname \*.mkv \) | sort -R | head -n 1)
     if [ ! -f "$fullPath" ]
     then
