cbebb4d95c4d467caf3523918ddc761fb055e522
josh <joshua2allen@gmail.com>
2023-07-15 17:05:00 -0500

    fixed bash error again

--- scripts/re-encode-process.sh
+++ scripts/re-encode-process.sh
@@ -40,6 +40,7 @@ then
     ffmpegOptions='-c:v libx264 -crf 30 -vf scale=-1:720 -movflags faststart -preset veryfast'
     fullPath=$(find $inputPath -type f \( -iname \*.mp4 -o -iname \*.webm -o -iname \*.avi -o -iname \*.mkv \) | sort -R | head -n 1)
     if [ ! -f "$fullPath" ]
+    then
         echo no video files found in $inputPath
         exit 0
     fi
