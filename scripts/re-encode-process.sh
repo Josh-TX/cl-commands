52ab0279981c10e45bf155e1be73444e876f9579
josh <joshua2allen@gmail.com>
2023-07-15 19:56:32 -0500

    added if check for input-720

--- scripts/re-encode-process.sh
+++ scripts/re-encode-process.sh
@@ -37,12 +37,15 @@ if [ ! -f "$fullPath" ]
 then 
     echo no video files found in $inputPath
     inputPath=$basePath/input-720
-    ffmpegOptions='-c:v libx264 -crf 28 -vf scale=-1:720 -movflags faststart -preset veryfast'
-    fullPath=$(find $inputPath -type f \( -iname \*.mp4 -o -iname \*.webm -o -iname \*.avi -o -iname \*.mkv \) | sort -R | head -n 1)
-    if [ ! -f "$fullPath" ]
+    if [ -d "$inputPath" ]
     then
-        echo no video files found in $inputPath
-        exit 0
+        ffmpegOptions='-c:v libx264 -crf 28 -vf scale=-1:720 -movflags faststart -preset veryfast'
+        fullPath=$(find $inputPath -type f \( -iname \*.mp4 -o -iname \*.webm -o -iname \*.avi -o -iname \*.mkv \) | sort -R | head -n 1)
+        if [ ! -f "$fullPath" ]
+        then
+            echo no video files found in $inputPath
+            exit 0
+        fi
     fi
 fi
 echo $fullPath > $tempFile
